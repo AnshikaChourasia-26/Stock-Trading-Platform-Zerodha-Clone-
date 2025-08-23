9e266135356d7990fc1173de727df7631c0203fa
Anshika chourasia <anshikachourasia0@gmail.com>
2025-08-23 23:02:03 +0530

    Add files via upload

---
+++ frontend/src/landingPage/test/Hero.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import {render, screen} from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+import Hero from "../home/Hero";
+
+//Test suite
+describe('Hero component', ()=>{
+    test('renders hero image', ()=>{
+        render(<Hero/>);
+        const heroImage = screen.getByAltText("Hero Image"); 
+        expect(heroImage).toBeInTheDocument();
+        expect(heroImage).toHaveAttribute("src", "media/images/homeHero.png");
+
+    });
+    test('renders signup button', ()=>{
+        render(<Hero/>);
+        const signupButton = screen.getByRole("button", {name: "/signup now/"}); 
+        expect(signupButton).toBeInTheDocument();
+        expect(signupButton).toHaveClass("btn-primary");
+
+    });
+});
